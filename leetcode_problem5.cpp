1f31080495f575ac2ee99c9d0b942a142fb7a6fc
Sahil036sharma <157585222+Sahil036sharma@users.noreply.github.com>
2024-07-05 19:36:26 +0530

    Add files via upload

---
+++ leetcode_problem5.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            std::reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    
+   vector<std::vector<int>> matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    solution.rotate(matrix1);
+    // Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+    vector<vector<int>> matrix2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    solution.rotate(matrix2);
+    // Output: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+
+    return 0;
+}
