74b6239e99a4eb777396a2ac5bdfc5f10b3cd7e2
Sahil036sharma <157585222+Sahil036sharma@users.noreply.github.com>
2024-07-18 13:53:45 +0530

    Add files via upload

---
+++ 155. min stack.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+public:
+stack<int>s,ss;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(ss.empty()){
+            ss.push(val);
+        }
+
+        else if(ss.top()>=val){
+            ss.push(val);
+
+        }
+    }
+    
+    void pop() {
+        if(s.top()==ss.top()){
+            ss.pop();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+        
+    }
+    
+    int getMin() {
+        return ss.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
