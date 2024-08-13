5b90471267a3e77afdcaa86ab6e7764fcaea65a6
Sahil036sharma <157585222+Sahil036sharma@users.noreply.github.com>
2024-08-13 21:26:26 +0530

    Add files via upload

---
+++ 107. Binary tree level order traversal 2.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void level(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i = sz-1; i>=0; i--){
+                TreeNode* f = q.front();
+                q.pop();
+                cout << f->val << " ";
+                if(f->left != NULL) q.push(f->left);
+                if(f->right != NULL) q.push(f->right);
+            }
+        }
+    }
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root == NULL) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            vector<int> levelNodes;
+            for(int i = sz-1; i>=0; i--){
+                TreeNode* f = q.front();
+                q.pop();
+                levelNodes.push_back(f->val);
+                if(f->left != NULL) q.push(f->left);
+                if(f->right != NULL) q.push(f->right);
+            }
+            
+            ans.push_back(levelNodes);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
