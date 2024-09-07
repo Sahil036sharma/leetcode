8f36b3808761eacbb323f27afda5f4621dc323e8
Sahil036sharma <157585222+Sahil036sharma@users.noreply.github.com>
2024-09-07 20:17:42 +0530

    Add files via upload

---
+++ 1367.linked list in binary tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if (root == nullptr) return false;
+        return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+
+    bool dfs(ListNode* head, TreeNode* root) {
+        if (head == nullptr) return true;
+        if (root == nullptr) return false;
+        if (head->val != root->val) return false;
+        return dfs(head->next, root->left) || dfs(head->next, root->right);
+    }
+};
