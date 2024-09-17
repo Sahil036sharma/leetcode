4d7a95ace81f9b839046fe72ff84ad84aea4c577
Sahil036sharma <157585222+Sahil036sharma@users.noreply.github.com>
2024-09-18 00:02:07 +0530

    Add files via upload

---
+++ 27.remove element.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
