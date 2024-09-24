bd3a759581514599658a3f4ec5a6b32135653a93
Sahil036sharma <157585222+Sahil036sharma@users.noreply.github.com>
2024-09-24 21:24:22 +0530

    Add files via upload

---
+++ 1480.Running sum of 1d array.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        return nums;
+    }
+};
